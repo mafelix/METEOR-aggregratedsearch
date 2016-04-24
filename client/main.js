644d7572acda88612112c11a63b829cf1b6fbe62
felix <felixmdev@gmail.com>
2016-04-23 22:52:28 -0700

    added to gitignore

--- client/main.js
+++ client/main.js
@@ -2,21 +2,18 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '.kpop/server/main.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+// subscribing grabbing data from main.js History MongoDB collection
+if(Meteor.isClient){
+  Meteor.subscribe('searchHistory');
+  return History.find();
+  
+  // template function to use History MongoDB collection
+  Template.searchHistory.helpers({
+    histories: function() {
+      console.log('we are in the helper');
+      Histories.find();
+    }
+  })
+}
--- server/main.js
+++ server/main.js
@@ -1,5 +1,25 @@
 import { Meteor } from 'meteor/meteor';
 
+Histories = new Mongo.Collection('histories');
+
 Meteor.startup(() => {
-  // code to run on server at startup
+  if( Meteor.isServer){
+    console.log('we are in the server');
+    // console.log(Histories);
+    if( Histories.find().count() == 0){
+      var list = [
+      { history: 'SNSD'},
+      { history: 'yaemin'},
+      { history: 'Exo'}
+      ];
+      
+      for(var i = 0; i < list.length; i++){
+        Histories.insert(list[i]);
+      }
+    }
+  // publishing Histories to client side main.js
+  Meteor.publish('searchHistory', function(){
+    return Histories.find();
+  })
+  }
 });
